3bfc3c5e0bf50004e9c4e81f767cdc59a5e28976
TheJoaoVitorio <joaovtr999x@gmail.com>
2024-06-02 12:08:08 -0300

    update script da receita

--- templates/static/js/c_receitaScript.js
+++ templates/static/js/c_receitaScript.js
@@ -1,54 +1,66 @@
 const nextButton = document.querySelector('.btn-proximo');
 const prevButton = document.querySelector('.btn-voltar');
-const steps      = document.querySelectorAll('.step');
-const formSteps  = document.querySelectorAll('.form-step');
+const steps = document.querySelectorAll('.step');
+const formSteps = document.querySelectorAll('.form-step');
+const lines = document.querySelectorAll('.line');
 
 let active = 1;
 
-nextButton.addEventListener('click', ()=>{
+nextButton.addEventListener('click', () => {
     active++;
-    if (active > steps.length){
+    if (active > steps.length) {
         active = steps.length;
     }
     updateProgress();
-})
+});
 
-prevButton.addEventListener('click', ()=>{
+prevButton.addEventListener('click', () => {
     active--;
-    if (active < 1){
+    if (active < 1) {
         active = 1;
     }
     updateProgress();
-})
+});
 
-const updateProgress = ()=>{
+const updateProgress = () => {
     console.log('steps.length =>' + steps.length);
-    console.log('active =>'+ active);
+    console.log('active =>' + active);
 
-    steps.forEach((step,i) => {
-        if (i == (active-1)){
+    steps.forEach((step, i) => {
+        if (i < active) {
             step.classList.add('iconStepActive');
-            formSteps[i].classList.add('ativo');
-            console.log('i => '+i);
-        }else{
+        } else {
             step.classList.remove('iconStepActive');
-            formSteps[i].classList.remove('ativo');
         }
     });
 
-    if (active === 1){
+    formSteps.forEach((formStep, i) => {
+        if (i === (active - 1)) {
+            formStep.classList.add('ativo');
+        } else {
+            formStep.classList.remove('ativo');
+        }
+    });
+
+    lines.forEach((line, i) => {
+        if (i < (active - 1)) {
+            line.classList.add('activeLine');
+        } else {
+            line.classList.remove('activeLine');
+        }
+    });
+
+    if (active === 1) {
         prevButton.disabled = true;
-    }else if (active === steps.length){
+    } else if (active === steps.length) {
         nextButton.disabled = true;
-    }else{
+    } else {
         prevButton.disabled = false;
         nextButton.disabled = false;
     }
-}
+};
 
-
-
-function dropDownReceita(){
+function dropDownReceita() {
     //form two
     const dropReceita = document.querySelector('.content-dropReceita');
     const selectBtn = document.querySelector('.select-btn');
@@ -64,13 +76,23 @@ function dropDownReceita(){
     const selectBtn3 = document.querySelector('.select-btn-frm3');
     const rotationIcon3 = selectBtn3.querySelector('i');
 
-    selectBtn3.addEventListener('click', ()=>{
+    selectBtn3.addEventListener('click', () => {
         dropReceitaAle.classList.toggle('activeDrop');
         rotationIcon3.classList.toggle('rotation-icon');
     });
 
+    //form four
+    const dropReceitaTiposRotulos = document.querySelector('.contentTiposRotulos');
+    const selectBtn4 = document.querySelector('.select-btn-frm4');
+    const rotationIcon4 = selectBtn4.querySelector('i');
+
+    selectBtn4.addEventListener('click', () => {
+        dropReceitaTiposRotulos.classList.toggle('activeDrop');
+        rotationIcon4.classList.toggle('rotation-icon');
+    });
 }
 
-    document.addEventListener('DOMContentLoaded' , () =>{
-        dropDownReceita();
-    });
+document.addEventListener('DOMContentLoaded', () => {
+    dropDownReceita();
+    updateProgress();
+});
