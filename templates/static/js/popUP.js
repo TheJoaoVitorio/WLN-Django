4418821f047d79e0d3082b5949f8e4e9f3ebf409
TheJoaoVitorio <joaovtr999x@gmail.com>
2024-07-18 20:24:50 -0300

    loading na sessao de contate-nos 100% funcionando

--- templates/static/js/popUP.js
+++ templates/static/js/popUP.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('#formularioContateNos');
-    form.addEventListener('submit', function() {
-        document.getElementById('email-form').style.display = 'none';
-        document.getElementById('loading-indicator').style.display = 'block';
-    });
+    const form = document.getElementById('formularioContateNos');
+    const loadingElements = document.getElementsByClassName('spinner-background');
+    
+    if (form) {
+        form.addEventListener('submit', function(event) {
+            const assunto = document.getElementById('assunto-contate-nos').value;
+            const mensagem = document.getElementById('mensagemContate-nos').value;
+            
+            if (assunto && mensagem) {
+                console.log("loading");
+                if (loadingElements.length > 0) {
+                    const loading = loadingElements[0];
+                    loading.style.display = 'flex';
+                }
+            } else {
+                event.preventDefault();
+                alert('Por favor, preencha todos os campos obrigatórios.');
+            }
+        });
+    }
 });
