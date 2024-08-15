aaa513741b320f58245ace6a2598ebd4671be9e2
JH0nnYGb <silvajoneffe@gmail.com>
2024-08-15 19:40:49 -0300

    telaTablet

--- templates/static/js/show-btn.js
+++ templates/static/js/show-btn.js
@@ -1,6 +1,6 @@
 function toggleText(button) {
     const textContainer = button.parentElement;
-    const receitaNome = button.getAttribute('data-receita');
+    const receitaNome = button.getAttribute('data-receita');    
     const fullTextElement = textContainer.querySelector('.full-text');
     const truncatedTextElement = textContainer.querySelector('.truncated-text');
     const receitaNomeElement = textContainer.querySelector('.receita-nome');
@@ -11,15 +11,19 @@ function toggleText(button) {
     textContainer.classList.toggle('show-full');
 
     if (textContainer.classList.contains('show-full')) {
+        fullTextElement.classList.add('show');
+        truncatedTextElement.classList.add('hide');
         fullTextElement.style.display = 'inline';
         truncatedTextElement.style.display = 'none';
         button.innerHTML = `<img id="tresbtn" src="${imgOpen}" alt="Mostrar menos">`;
         receitaNomeElement.textContent = receitaNome;
         receitaNomeElement.style.display = 'block';
     } else {
+        fullTextElement.classList.remove('show');
+        truncatedTextElement.classList.remove('hide');
         fullTextElement.style.display = 'none';
         truncatedTextElement.style.display = 'inline';
         button.innerHTML = `<img id="tresbtn" src="${imgClosed}" alt="Mostrar mais">`;
         receitaNomeElement.style.display = 'none';
     }
-}
+}   
