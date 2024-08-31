63234f5ecc166342e5ebdf0253e6cf7c85147cb6
Joneffe_Jhonny <160853946+JH0nnYGb@users.noreply.github.com>
2024-08-31 19:14:51 -0300

    Revert "add ingrediente nomeP ( incompleto )"

--- templates/static/js/get_meusingredientes.js
+++ templates/static/js/get_meusingredientes.js
@@ -5,22 +5,15 @@ document.getElementById('MeusIngredientesCheck').addEventListener('change', func
         .then(data => {
             if (data.MeusIngredientesList){
                 const ListaIngredientes = document.getElementById('optionsIngredientes');//ul para add as li
-                const inputField = document.getElementById('');//search 
+                const inputField = document.getElementById('BuscaIngrediente');//search 
 
                 inputField.addEventListener('input', function() {
                     const searchTerm = this.value.toLowerCase();
                     ListaIngredientes.innerHTML = '';
                     data.MeusIngredientesList.forEach(ingrediente => {
-                       
-                        console.log(nomeExibido)
-                        
-                        if (nomeExibido.toLowerCase().includes(searchTerm)) {
-                            if (nomeExibido.length > 10){
-                                nomeExibido = nomeExibido.substring(0, 10) + '...'; 
-                            }
-                            
+                        if (ingrediente.nomeIngrediente.toLowerCase().includes(searchTerm)) {
                             const item = document.createElement('li');
-                            item.textContent = nomeExibido;
+                            item.textContent = ingrediente.nomeIngrediente;
                             item.dataset.idIngrediente = ingrediente.id;
                             ListaIngredientes.appendChild(item);
                         }
@@ -30,18 +23,11 @@ document.getElementById('MeusIngredientesCheck').addEventListener('change', func
                 // Inicializa a lista com todos os ingredientes
                 ListaIngredientes.innerHTML = '';
                 data.MeusIngredientesList.forEach(ingrediente =>{
-                    let nomeExibido = ingrediente.nomeIngrediente;
-                    
-                    if (nomeExibido.length > 10) {
-                        nomeExibido = nomeExibido.substring(0, 10) + '...';
-                    }
-                
                     const item = document.createElement('li');
-                    item.textContent = nomeExibido;
+                    item.textContent = ingrediente.nomeIngrediente;
                     item.dataset.idIngrediente = ingrediente.id;
                     ListaIngredientes.appendChild(item);
                     console.log(item.idIngrediente)
-                    
                 });
             } else if (data.erro){
                 alert(data.erro);
